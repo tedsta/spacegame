b0b75c06f0f21e9924f223890c8b587f8005ec65
Brian Hall <bhall7@hawaii.edu>
2014-06-20 12:35:38 -1000

    pathfinding works but is wayyyyy slow, what up

--- src/path.py
+++ src/path.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 import sys
+import time
 from collections import namedtuple
-
-SquareInfo = namedtuple("SquareInfo", "parent mov_cost manhattan_dist is_open")
 
 def find_path(grid, position, destination):
     """Returns path as a list of tuples.
@@ -16,13 +15,47 @@ def find_path(grid, position, destination):
         position: an (x, y) tuple
         destination: an (x, y) tuple
     """
-    path = [position, destination]
+    if position == destination:
+        return [position, destination]
+    # Make a dict of dicts whose keys are (x, y) tuples
+    #      and whose values are {parent: __, mov_cost: __,
+    #      man_dist: __, is_open: __} dicts
+    path = []
     squares = {}
+    # Initialize...
     # Add start position to closed list.
-    squares[position] = SquareInfo(None, 0,
-            _manhattan_distance(position, destination), False)
+    squares[position] = {"parent": None, "mov_cost": 0,
+            "man_dist": _manhattan_distance(position, destination),
+            "is_open": False}
+
     moves = _get_available_moves(grid, position)
-    print(moves)
+    # Add all available moves to open list with start position as parent
+    # ***for each position in available moves, calculate score
+        # score = movement cost to that square + manhattan dist from that square to dest
+    for move in moves:
+        mov_cost = _get_mov_cost(position, move)
+        man_dist = _manhattan_distance(move, destination)
+        squares[move] = {"parent": position, "mov_cost": mov_cost, 
+                "man_dist": man_dist, "is_open": True}
+
+    current_position = position
+    while destination not in squares:
+        current_position = _next_move(grid, current_position, destination, squares)
+
+    # Build path starting at destination and moving backwards:
+    current_position = destination
+    path.append(current_position)
+
+    while position not in path:
+        time.sleep(1)
+        path.append(squares[current_position]["parent"])
+        current_position = squares[current_position]["parent"]
+
+    path = list(reversed(path))
+    return path
+
+def _next_move(grid, position, destination, squares):
+    moves = _get_available_moves(grid, position)
     min_score = 0
     best_move = None
     # Add all available moves to open list with start position as parent
@@ -31,39 +64,49 @@ def find_path(grid, position, destination):
     for move in moves:
         mov_cost = _get_mov_cost(position, move)
         man_dist = _manhattan_distance(move, destination)
-        squares[move] = SquareInfo(position, mov_cost, man_dist, True)
+        if move not in squares:
+            squares[move] = {"parent": position, "mov_cost": mov_cost, 
+                    "man_dist": man_dist, "is_open": True}
         score = mov_cost + man_dist
         if min_score == 0 or score < min_score:
             best_move = move
+            min_score = score
     if not min_score or not best_move:
         # Something's wrong here
         sys.stderr.write("error in path finding at " + str(position) + "\n")
         sys.stderr.write("moves look like this: " + str(moves) + "\n")
         return path
+    new_position = best_move
     # choose position with lowest score
     # drop that position from open list and add it to closed list
-    squares[best_move].is_open = False
+    squares[new_position]["is_open"] = False
     # check all its available moves (don't count those on closed list)
-    moves = _get_available_moves(grid, best_move)
+    moves = _get_available_moves(grid, new_position)
     # for any available move already on the open list, if its movement cost is lower from
     #    square then update its parent to be current square. (that is, if it's cheaper to move
     #    to current square and then to new square than to just go straight to that square)
     for move in moves:
-        if squares[move].is_open:
-            if squares[move].mov_cost < squares[best_move].mov_cost + _get_mov_cost(best_move, move):
-                pass
-                # change parent to bestmove
+        if move in squares:
+            if squares[move]["is_open"]: 
+                old_cost = squares[move]["mov_cost"]
+                new_cost = squares[new_position]["mov_cost"] +\
+                    _get_mov_cost(new_position, move)
+                if new_cost < old_cost:
+                    squares[move]["parent"] = new_position
         else:
             # add newly available moves to open list if relevant, with new position as parent
-            squares[move].is_open = True
+            mov_cost = _get_mov_cost(new_position, move)
+            man_dist = _manhattan_distance(move, destination)
+            squares[move] = {"parent": new_position, "mov_cost": mov_cost, 
+                    "man_dist": man_dist, "is_open": True}
+    return new_position
 
-    # anyway, after that continue (e.g. start at ***) until destination has a parent.
-    # once destination has a parent, reverse route is dest, dest_parent, dest_parent_parent, etc. congrats
-
-    path.append(destination)
-    return path
 
 def _get_available_moves(grid, position):
+    """Returns a list of allowable moves from a given position.
+
+    Requires that the grid.get() returns a WalkDirs namedtuple.
+    """
     x = position[0]
     y = position[1]
     moves = []
@@ -98,7 +141,7 @@ def _manhattan_distance(position, destination):
     pos_y = position[1]
     dest_x = destination[0]
     dest_y = destination[1]
-    distance = abs(pos_x - dest_x) + abs(pos_y - dest_y)
+    distance = 10*abs(pos_x - dest_x) + 10*abs(pos_y - dest_y)
     return distance
 
 def _get_mov_cost(position, destination):
--- test/path_tests.py
+++ test/path_tests.py
@@ -8,11 +8,29 @@ from src.grid import Grid, WalkDirs
 class TestPath(unittest.TestCase):
 
     def setUp(self):
+        """Defines a 4x3 grid with the following layout:
+
+        (0=walkable, X=not walkable, P=position, D=destination)
+
+        0 0 0 D
+        0 P X 0
+        X X X X
+        """
         self.grid = Grid(4, 3)
-        self.upwalkdirs = WalkDirs(up_left=True, up=True, up_right=True,
-                    left=True, right=True, down_left=False,
+        pwalkdirs = WalkDirs(up_left=True, up=True, up_right=False,
+                    left=True, right=False, down_left=False,
                     down=False, down_right=False)
-        self.grid.set(1, 1, self.upwalkdirs)
+        self.grid.set(1, 1, pwalkdirs)
+        walkdirs_0_0 = WalkDirs(False, False, False, False, True, False, True, True)
+        self.grid.set(0, 0, walkdirs_0_0)
+        walkdirs_0_1 = WalkDirs(False, True, True, False, True, False, False, False)
+        self.grid.set(0, 1, walkdirs_0_1)
+        walkdirs_1_0 = WalkDirs(False, False, False, True, True, True, True, False)
+        self.grid.set(1, 0, walkdirs_1_0)
+        walkdirs_2_0 = WalkDirs(False, False, False, True, True, False, False, False)
+        self.grid.set(2, 0, walkdirs_2_0)
+        walkdirs_3_0 = WalkDirs(False, False, False, True, False, False, True, False)
+        self.grid.set(3, 0, walkdirs_3_0)
         self.position = (1, 1)
 
     def test_path_when_destination_is_same_as_position(self):
@@ -27,9 +45,9 @@ class TestPath(unittest.TestCase):
         actual = find_path(self.grid, self.position, destination)
         self.assertEqual(actual, expected)
         
-    def test_path_one_square_right(self):
-        destination = (2, 1)
-        expected = [(1, 1), (2, 1)]
+    def test_path_one_square_left(self):
+        destination = (0, 1)
+        expected = [(1, 1), (0, 1)]
         actual = find_path(self.grid, self.position, destination)
         self.assertEqual(actual, expected)
         
@@ -40,20 +58,26 @@ class TestPath(unittest.TestCase):
         self.assertEqual(actual, expected)
 
     def test_path_two_squares_right(self):
-        destination = (3, 1)
-        expected = [(1, 1), (2, 1), (3, 1)]
+        position = (0, 0)
+        destination = (2, 0)
+        expected = [(0, 0), (1, 0), (2, 0)]
+        actual = find_path(self.grid, position, destination)
+        self.assertEqual(actual, expected)
+
+    def test_whole_path(self):
+        destination = (3, 0)
+        expected = [(1, 1), (1, 0), (2, 0), (3, 0)]
         actual = find_path(self.grid, self.position, destination)
-        # TODO
-        #self.assertEqual(actual, expected)"""
+        self.assertEqual(actual, expected)
    
     def test_get_available_moves(self):
-        expected = [(0, 0), (1, 0), (2, 0), (0, 1), (2, 1)]
+        expected = [(0, 0), (1, 0), (0, 1)]
         actual = _get_available_moves(self.grid, self.position)
         self.assertEqual(actual, expected)
 
     def test_manhattan_distance(self):
         destination = (4, 2)
-        expected = 4
+        expected = 40
         actual = _manhattan_distance(self.position, destination)
         self.assertEqual(actual, expected)
 
